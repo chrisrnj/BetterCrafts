7c711aa6a8a23ae892ee1f293b24a52a80c352e7
Epicnicity322 <christianorangel322@gmail.com>
2023-08-29 18:21:45 -0300

    Add IndependentVersionHandler

---
+++ src/main/java/me/chickenstyle/crafts/Main.java
@@ -0,0 +1,91 @@
+package me.chickenstyle.crafts;
+
+import com.epicnicity322.epicpluginlib.core.EpicPluginLib;
+import me.chickenstyle.crafts.configs.AltarCrafts;
+import me.chickenstyle.crafts.events.ArmorStandInteractEvent;
+import me.chickenstyle.crafts.events.InteractEvent;
+import me.chickenstyle.crafts.events.InventoryEvents;
+import me.chickenstyle.crafts.events.SendMessageEvent;
+import me.chickenstyle.crafts.versions.IndependentVersionHandler;
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+public class Main extends JavaPlugin {
+    public static HashMap<UUID, Recipe> creatingRecipe;
+    public static HashMap<UUID, Integer> animationNumber;
+    public static Set<UUID> typeID;
+    public static Set<UUID> opening;
+    private static NMSHandler versionHandler;
+    private static Main instance;
+
+    public static NMSHandler getNMSHandler() {
+        return versionHandler;
+    }
+
+    public static Main getInstance() {
+        return instance;
+    }
+
+    @Override
+    public void onEnable() {
+
+        //Load Stuff
+        instance = this;
+        creatingRecipe = new HashMap<UUID, Recipe>();
+        animationNumber = new HashMap<UUID, Integer>();
+        typeID = new HashSet<UUID>();
+        opening = new HashSet<UUID>();
+
+        //Load configs
+        this.getConfig().options().copyDefaults();
+        saveDefaultConfig();
+        new AltarCrafts(this);
+        saveResource("lang.yml", false);
+
+        //Check server version
+        if (!getServerVersion()) {
+            getServer().getConsoleSender().sendMessage(ChatColor.RED + "BetterCrafts >>> Plugin will be disabled!");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+
+        loadListeners();
+
+        //Load commands
+        getCommand("bettercrafts").setExecutor(new BetterCraftsCommand());
+        getCommand("bettercrafts").setTabCompleter(new CraftsTab());
+        getCommand("recipes").setExecutor(new RecipesCommand());
+
+        //Getting data
+        int pluginId = 8954;
+        Metrics metrics = new Metrics(this, pluginId);
+        metrics.addCustomChart(new Metrics.SimplePie("chart_id", () -> "My value"));
+
+        System.out.println(Utils.color("&aBetterCraft has been enabled!"));
+    }
+
+    public boolean getServerVersion() {
+        try {
+            versionHandler = new IndependentVersionHandler();
+            getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "BetterCrafts >>> NMS Version Detected: " + EpicPluginLib.Platform.getVersion());
+            return true;
+        } catch (Throwable t) {
+            t.printStackTrace();
+            getServer().getConsoleSender().sendMessage(ChatColor.RED + "BetterCrafts >>> This version isn't supported!");
+            return false;
+        }
+    }
+
+    public void loadListeners() {
+        getServer().getPluginManager().registerEvents(new InventoryEvents(), this);
+        getServer().getPluginManager().registerEvents(new SendMessageEvent(), this);
+        getServer().getPluginManager().registerEvents(new InteractEvent(), this);
+        getServer().getPluginManager().registerEvents(new ArmorStandInteractEvent(), this);
+    }
+
+}
---
+++ src/main/java/me/chickenstyle/crafts/versions/IndependentVersionHandler.java
@@ -0,0 +1,86 @@
+package me.chickenstyle.crafts.versions;
+
+import com.epicnicity322.epicpluginlib.bukkit.reflection.ReflectionUtil;
+import com.epicnicity322.epicpluginlib.bukkit.reflection.type.DataType;
+import com.epicnicity322.epicpluginlib.bukkit.reflection.type.SubPackageType;
+import me.chickenstyle.crafts.NMSHandler;
+import net.minecraft.nbt.NBTTagCompound;
+import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.Objects;
+
+public class IndependentVersionHandler implements NMSHandler {
+    private final @NotNull Class<?> craftItemStack = Objects.requireNonNull(ReflectionUtil.getClass("CraftItemStack", SubPackageType.INVENTORY));
+    private final @NotNull Method asNMSCopy = Objects.requireNonNull(ReflectionUtil.getMethod(craftItemStack, "asNMSCopy", ItemStack.class));
+    private final @NotNull Method asBukkitCopy = Objects.requireNonNull(ReflectionUtil.getMethod(craftItemStack, "asBukkitCopy", ItemStack.class));
+    private final @NotNull Method setInt = findMethodByTypeAndParameters(Void.TYPE, String.class, int.class);
+    private final @NotNull Method getInt = findMethodByTypeAndParameters(int.class, String.class);
+    private final @NotNull Field tagField = Objects.requireNonNull(ReflectionUtil.findFieldByType(net.minecraft.world.item.ItemStack.class, NBTTagCompound.class, false));
+
+    private @NotNull Method findMethodByTypeAndParameters(Class<?> returnType, Class<?>... parameters) {
+        Method method = null;
+
+        for (Method m : NBTTagCompound.class.getMethods()) {
+            if (m.getReturnType().equals(returnType) && DataType.equalsArray(m.getParameterTypes(), parameters)) {
+                method = m;
+                break;
+            }
+        }
+
+        assert method != null;
+        return method;
+    }
+
+    @Override
+    public ItemStack addIDTag(ItemStack item, int data) {
+        try {
+            net.minecraft.world.item.ItemStack nmsItem = (net.minecraft.world.item.ItemStack) asNMSCopy.invoke(null, item);
+            NBTTagCompound itemCompound = (NBTTagCompound) tagField.get(nmsItem);
+
+            if (itemCompound == null) {
+                itemCompound = new NBTTagCompound();
+                tagField.set(nmsItem, itemCompound);
+            }
+
+            setInt.invoke(itemCompound, "IDTag", data);
+            return (ItemStack) asBukkitCopy.invoke(null, nmsItem);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
+    @Override
+    public boolean hasIDTag(ItemStack item) {
+        try {
+            net.minecraft.world.item.ItemStack nmsItem = (net.minecraft.world.item.ItemStack) asNMSCopy.invoke(null, item);
+            NBTTagCompound itemCompound = (NBTTagCompound) tagField.get(nmsItem);
+
+            if (itemCompound == null) return false;
+
+            return getInt.invoke(itemCompound, "IDTag") != null;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    @Override
+    public int getID(ItemStack item) {
+        try {
+            net.minecraft.world.item.ItemStack nmsItem = (net.minecraft.world.item.ItemStack) asNMSCopy.invoke(null, item);
+            NBTTagCompound itemCompound = (NBTTagCompound) tagField.get(nmsItem);
+
+            if (itemCompound == null) return 0;
+
+            return (int) getInt.invoke(itemCompound, "IDTag");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return 0;
+        }
+    }
+}
